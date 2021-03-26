b9e07f8e84a6e2cb280a4c5435adfd9fbb9f1536
yashdesai <yddc902@live.com>
2021-03-26 20:20:13 +0530

    error resolved

--- css/env.php
+++
@@ -1,9 +0,0 @@
-<?php  
-    $app_name = "Pooja Electricals";
-
-    //Email admin setup
-
-    $fromName = "Pooja Elecricals";
-    $from = "poojaelectricals462@gmail.com";
-    $password = "Elect#2021";
-?>
--- css/user/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if (isset($_SESSION['useremail'])) {
-        session_destroy();
-        // header("location: ./userLogin.php");
-    }
-    header("location: ./userLogin.php");
-?>
